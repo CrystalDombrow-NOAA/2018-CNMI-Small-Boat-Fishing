1a8e726ffb508de8ebf7aede58ab3b450ec80282
Crystal Dombrow <94503492+CrystalDombrow-NOAA@users.noreply.github.com>
2025-03-27 15:36:26 -1000

    Add files via upload

--- 11_FishingVendors.R
+++ 11_FishingVendors.R
@@ -29,10 +29,20 @@ source("Functions/DistributionFunction_MultipleVariables_WhereSell.R")
 
 # General crew stats
 vendor.stats <- cnmi.data.cleaned %>% 
-  select(Survey, boat.owner, vendor, primary.target, Q1.mid.ifelse,
+  select(Survey, boat.owner, highliner, vendor, primary.target, Q1.mid.ifelse,
          Q8A.mid.ifelse:Q8C.mid.ifelse, Q9.mid, Q11A.yesno:Q11G.yesno, Q12A, 
-         Q17.dummy.total, Q35A:Q35E, Q38A:Q38E, Q42:Q53) %>% 
-  filter(vendor == "vendor")
+         Q17.dummy.total, Q35A:Q35E, Q38A:Q38E, Q42:Q53, Q22.mid) %>% 
+  #filter(boat.owner == "not boat owner") %/%
+  filter(vendor == "vendor") %>%
+  mutate(Q8A.mean = round(mean(Q8A.mid.ifelse, na.rm = T), 1),
+         Q8B.mean = round(mean(Q8B.mid.ifelse, na.rm = T), 1),
+         Q8C.mean = round(mean(Q8C.mid.ifelse, na.rm = T), 1))
+
+
+#---------------------------------------------------------------------------
+
+# Q8. Lbs of fish caught
+
 
 
 #---------------------------------------------------------------------------
@@ -47,14 +57,15 @@ q14.vendor.data <- cnmi.data.cleaned %>%
 q14.vendor.percents <- cnmi.data.cleaned %>% 
   select(vendor, Q14A:Q14F, Q14.multiple.responses) %>%
   filter(vendor == "vendor") %>%
-  mutate(q14a.per = round(sum(Q14A) / nrow(q14.data) * 100, 1),
+  mutate(q14a.per = round(sum(Q14A) / nrow(q14.vendor.data) * 100, 1),
          q14b.mean = round(mean(Q14B, na.rm = T), 1),
          q14c.mean = round(mean(Q14C, na.rm = T), 1),
-         q14d.per = round(sum(Q14D) / nrow(q14.data) * 100, 1),
-         q14e.per = round(sum(Q14E) / nrow(q14.data) * 100, 1),
-         q14.mult.per = round(sum(Q14.multiple.responses) / nrow(q14.data) * 100, 1),
+         q14d.per = round(sum(Q14D) / nrow(q14.vendor.data) * 100, 1),
+         q14e.per = round(sum(Q14E) / nrow(q14.vendor.data) * 100, 1),
+         q14.mult.per = round(sum(Q14.multiple.responses) / 
+                                nrow(q14.vendor.data) * 100, 1),
          kept.fish = ifelse(Q14A == 1 | !is.na(Q14B), 1, 0),
-         kept.fish.per = round(sum(kept.fish) / nrow(q14.data) * 100, 1))
+         kept.fish.per = round(sum(kept.fish) / nrow(q14.vendor.data) * 100, 1))
 
 
 #---------------------------------------------------------------------------
